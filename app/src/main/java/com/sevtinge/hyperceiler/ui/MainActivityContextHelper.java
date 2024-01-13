8432f86b168db07ad9acb16418bfa267d9091c26
Horange <horange@163.com>
2024-01-13 12:57:21 +0800

    Horange

--- app/src/main/java/com/sevtinge/hyperceiler/ui/MainActivityContextHelper.java
+++ app/src/main/java/com/sevtinge/hyperceiler/ui/MainActivityContextHelper.java
@@ -62,69 +62,8 @@ public class MainActivityContextHelper {
         return null;
     }
 
-    public boolean isSignCheckPass(){
-        ArrayList<String> signList = new ArrayList<>();
-        signList.add("46:4C:5C:9D:A2:8C:AE:E6:B3:28:8D:AE:13:2C:A3:6D:52:A1:64:89:E0:95:CF:7B:52:AC:A7:11:F0:93:82:3C");
-        signList.add("79:04:4B:BC:29:6B:E1:1A:9E:33:84:C4:91:F1:AD:D1:C0:CA:EE:CE:22:B9:24:FD:5B:7E:5A:14:C0:C3:99:60");
-        String sign = getSHA256Signature();
-        if (signList.contains(sign)) {
-            for (String element : signList) {
-                if (!sign.equals(element)) {
-                    break;
-                }
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean verifyDexCRC() {
-        String dexCrcStr = context.getResources().getString(R.string.crc_value);
-        if(dexCrcStr.startsWith("Error")) return false;
-        //long dexCrc = Long.parseLong("613BD799");
-
-        //String orginalCrc = getString(R.string.str_code);
-        ZipFile zf;
-        try {
-            zf = new ZipFile(context.getApplicationContext().getPackageCodePath());
-            ZipEntry ze = zf.getEntry("classes.dex");
-            long decCrc = ze.getCrc();
-            String strCrc = Long.toHexString(decCrc);
-            strCrc = strCrc.toUpperCase();
-            //String MD5Crc = MD5Util.GetMD5Code(strCrc);
-            //Log.e("checkcrc", MD5Crc);
-            if (dexCrcStr.equals(strCrc)) {
-                //ActivityManagerUtil.getScreenManager().removeAllActivity();
-                return true;
-            } else return false;
-        } catch (IOException e) {
-            return false;
-        }
-    }
-
-    public String getDexCRC() {
-        String dexCrcStr = context.getResources().getString(R.string.crc_value);
-        if(dexCrcStr.startsWith("Error")) return dexCrcStr;
-        //long dexCrc = Long.parseLong("613BD799");
-
-        //String orginalCrc = getString(R.string.str_code);
-        ZipFile zf;
-        try {
-            zf = new ZipFile(context.getApplicationContext().getPackageCodePath());
-            ZipEntry ze = zf.getEntry("classes.dex");
-            long decCrc = ze.getCrc();
-            String strCrc = Long.toHexString(decCrc);
-            strCrc = strCrc.toUpperCase();
-            //String MD5Crc = MD5Util.GetMD5Code(strCrc);
-            //Log.e("checkcrc", MD5Crc);
-            if (dexCrcStr.equals(strCrc)) {
-                //ActivityManagerUtil.getScreenManager().removeAllActivity();
-                return dexCrcStr + " = " + strCrc;
-            } else return dexCrcStr + " != " + strCrc;
-        } catch (IOException e) {
-            return e.toString();
-        }
+    public boolean isSignCheckPass() {
+        return false;
     }
 
     public String getRandomTip() {
